c893eb756037f3720471f4b95871db38e145659f
Mary <maryauma2606@gmail.com>
2025-07-04 17:10:20 +0300

    add initials

--- components/ErrorBoundary.tsx
+++ components/ErrorBoundary.tsx
@@ -1,36 +1,32 @@
-interface State {
-  hasError: boolean;
-}
+import React, { Component, ReactNode } from "react";
 
 interface ErrorBoundaryProps {
   children: ReactNode;
 }
 
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
 
-class ErrorBoundary extends React.Component<ErrorBoundaryProps , State> {
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
   constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error: Error): State {
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    // Update state so the next render shows the fallback UI
     return { hasError: true };
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    console.log({ error, errorInfo });
+    // You can also log the error to an error reporting service
+    console.error("ErrorBoundary caught an error:", error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
-      return (
-        <div>
-          <h2>Oops, there is an error!</h2>
-          <button onClick={() => this.setState({ hasError: false })}>
-            Try again?
-          </button>
-        </div>
-      );
+      return <h2>Something went wrong.</h2>;
     }
 
     return this.props.children;
